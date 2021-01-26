26133404b0c71c09e59557a816ec1491831b9fbb
ZackEnixey <zzftn90@gmail.com>
2021-01-27 00:29:47 +0100

    playing with input

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Input extends React.Component {
+
+    render(){
+        return(
+            <div> 
+
+                input
+            </div>
+        )
+    }
+}
